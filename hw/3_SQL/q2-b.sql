4c14d299e58e0315c1d29af9e7aeb4736c0d4ac1
Alga <zhuyunin@usc.edu>
2017-10-19 16:39:22 -0700

    Update q2-b.sql

--- hw/3_SQL/q2-b.sql
+++ hw/3_SQL/q2-b.sql
@@ -1,5 +1,5 @@
 CREATE VIEW horror_view AS
 SELECT DISTINCT cu.customer_id
 FROM customer cu, film_category f_c, film f, inventory i, rental r
-WHERE cu.customer_id = r.customer_id AND f.film_id = f_c.film_id AND f_c.category_id = 11 AND f.film_id = i.film_id AND i.inventory_id = r.inventory_id
+WHERE cu.customer_id = r.customer_id AND f.film_id = f_c.film_id AND f_c.category_id = 11 AND f.film_id = i.film_id AND i.inventory_id = r.inventory_id AND r.customer_id = customer.customer_id
 ORDER BY cu.customer_id;
