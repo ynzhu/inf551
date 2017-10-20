c6b2716505d0741dff748fb19b9a44d02f160343
Alga <zhuyunin@usc.edu>
2017-10-20 12:28:54 -0700

    Update q1-e.sql

--- hw/3_SQL/q1-e.sql
+++ hw/3_SQL/q1-e.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT CONCAT(first_name, " ", last_name) as name
+SELECT CONCAT(first_name, " ", last_name) as name
 FROM film_actor f_a, actor a
 WHERE f_a.actor_id = a.actor_id
 GROUP BY f_a.actor_id
