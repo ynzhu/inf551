b95105092ba040284c91efd12c1444251102cbf8
Alga <zhuyunin@usc.edu>
2017-10-20 01:08:28 -0700

    Update q1-e.sql

--- hw/3_SQL/q1-e.sql
+++ hw/3_SQL/q1-e.sql
@@ -1,6 +1,6 @@
-SELECT DISTINCT CONCAT(first_name, " ", last_name)
+SELECT DISTINCT CONCAT(first_name, " ", last_name) as name
 FROM film_actor f_a, actor a
 WHERE f_a.actor_id = a.actor_id
 GROUP BY f_a.actor_id
 HAVING count(f_a.film_id) > 1
-ORDER BY first_name ASC;
+ORDER BY name ASC;
